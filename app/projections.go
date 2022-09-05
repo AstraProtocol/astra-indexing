9b471430f83c148d311885d41e82c34d79ead02e
hoanguyenkh <hoa.nguyen2@tiki.vn>
2022-09-05 17:43:46 +0700

    init project

---
+++ app/projections.go
@@ -0,0 +1,243 @@
+package main
+
+import (
+	"strings"
+
+	"github.com/crypto-com/chain-indexing/appinterface/cosmosapp"
+	"github.com/crypto-com/chain-indexing/appinterface/rdb"
+	configuration "github.com/crypto-com/chain-indexing/bootstrap/config"
+	projection_entity "github.com/crypto-com/chain-indexing/entity/projection"
+	applogger "github.com/crypto-com/chain-indexing/external/logger"
+	cosmosapp_infrastructure "github.com/crypto-com/chain-indexing/infrastructure/cosmosapp"
+	"github.com/crypto-com/chain-indexing/infrastructure/pg"
+	"github.com/crypto-com/chain-indexing/infrastructure/pg/migrationhelper"
+	github_migrationhelper "github.com/crypto-com/chain-indexing/infrastructure/pg/migrationhelper/github"
+	"github.com/crypto-com/chain-indexing/projection/account"
+	"github.com/crypto-com/chain-indexing/projection/account_message"
+	"github.com/crypto-com/chain-indexing/projection/account_transaction"
+	"github.com/crypto-com/chain-indexing/projection/block"
+	"github.com/crypto-com/chain-indexing/projection/blockevent"
+	"github.com/crypto-com/chain-indexing/projection/chainstats"
+	"github.com/crypto-com/chain-indexing/projection/ibc_channel"
+	"github.com/crypto-com/chain-indexing/projection/ibc_channel_message"
+	"github.com/crypto-com/chain-indexing/projection/proposal"
+	"github.com/crypto-com/chain-indexing/projection/transaction"
+	"github.com/crypto-com/chain-indexing/projection/validator"
+	"github.com/crypto-com/chain-indexing/projection/validatorstats"
+)
+
+func initProjections(
+	logger applogger.Logger,
+	rdbConn rdb.Conn,
+	config *configuration.Config,
+	customConfig *CustomConfig,
+) []projection_entity.Projection {
+	if !config.IndexService.Enable {
+		return []projection_entity.Projection{}
+	}
+
+	var cosmosAppClient cosmosapp.Client
+	if config.CosmosApp.Insecure {
+		cosmosAppClient = cosmosapp_infrastructure.NewInsecureHTTPClient(
+			config.CosmosApp.HTTPRPCUrl, config.Blockchain.BondingDenom,
+		)
+	} else {
+		cosmosAppClient = cosmosapp_infrastructure.NewHTTPClient(
+			config.CosmosApp.HTTPRPCUrl, config.Blockchain.BondingDenom,
+		)
+	}
+
+	projections := make([]projection_entity.Projection, 0, len(config.IndexService.Projection.Enables))
+	initParams := InitProjectionParams{
+		Logger:  logger,
+		RdbConn: rdbConn,
+
+		ExtraConfigs: config.IndexService.Projection.ExtraConfigs,
+
+		CosmosAppClient:       cosmosAppClient,
+		AccountAddressPrefix:  config.Blockchain.AccountAddressPrefix,
+		ConsNodeAddressPrefix: config.Blockchain.ConNodeAddressPrefix,
+
+		GithubAPIUser:    config.IndexService.GithubAPI.Username,
+		GithubAPIToken:   config.IndexService.GithubAPI.Token,
+		MigrationRepoRef: config.IndexService.GithubAPI.MigrationRepoRef,
+
+		ServerMigrationRepoRef: customConfig.ServerGithubAPI.MigrationRepoRef,
+	}
+	for _, projectionName := range config.IndexService.Projection.Enables {
+		projection := InitProjection(
+			projectionName, initParams,
+		)
+		if projection == nil {
+			continue
+		} else {
+			if onInitErr := projection.OnInit(); onInitErr != nil {
+				logger.Errorf(
+					"error initializing projection %s, system will attempt to initialize the projection again on next restart: %v",
+					projection.Id(), onInitErr,
+				)
+				continue
+			}
+		}
+		projections = append(projections, projection)
+	}
+
+	logger.Infof("Enabled the follow projection: [%s]", strings.Join(config.IndexService.Projection.Enables, ", "))
+
+	return projections
+}
+
+func InitProjection(name string, params InitProjectionParams) projection_entity.Projection {
+	connString := params.RdbConn.(*pg.PgxConn).ConnString()
+
+	githubMigrationHelperConfig := github_migrationhelper.Config{
+		GithubAPIUser:    params.GithubAPIUser,
+		GithubAPIToken:   params.GithubAPIToken,
+		MigrationRepoRef: params.MigrationRepoRef,
+		ConnString:       connString,
+	}
+
+	switch name {
+	case "Account":
+		sourceURL := github_migrationhelper.GenerateDefaultSourceURL(name, githubMigrationHelperConfig)
+		databaseURL := migrationhelper.GenerateDefaultDatabaseURL(name, connString)
+		migrationHelper := github_migrationhelper.NewGithubMigrationHelper(sourceURL, databaseURL)
+
+		return account.NewAccount(params.Logger, params.RdbConn, params.CosmosAppClient, migrationHelper)
+	case "AccountTransaction":
+		sourceURL := github_migrationhelper.GenerateDefaultSourceURL(name, githubMigrationHelperConfig)
+		databaseURL := migrationhelper.GenerateDefaultDatabaseURL(name, connString)
+		migrationHelper := github_migrationhelper.NewGithubMigrationHelper(sourceURL, databaseURL)
+
+		return account_transaction.NewAccountTransaction(params.Logger, params.RdbConn, params.AccountAddressPrefix, migrationHelper)
+	case "AccountMessage":
+		sourceURL := github_migrationhelper.GenerateDefaultSourceURL(name, githubMigrationHelperConfig)
+		databaseURL := migrationhelper.GenerateDefaultDatabaseURL(name, connString)
+		migrationHelper := github_migrationhelper.NewGithubMigrationHelper(sourceURL, databaseURL)
+
+		return account_message.NewAccountMessage(params.Logger, params.RdbConn, params.AccountAddressPrefix, migrationHelper)
+	case "Block":
+		sourceURL := github_migrationhelper.GenerateDefaultSourceURL(name, githubMigrationHelperConfig)
+		databaseURL := migrationhelper.GenerateDefaultDatabaseURL(name, connString)
+		migrationHelper := github_migrationhelper.NewGithubMigrationHelper(sourceURL, databaseURL)
+
+		return block.NewBlock(params.Logger, params.RdbConn, migrationHelper)
+	case "BlockEvent":
+		sourceURL := github_migrationhelper.GenerateSourceURL(
+			github_migrationhelper.MIGRATION_GITHUB_URL_FORMAT,
+			params.GithubAPIUser,
+			params.GithubAPIToken,
+			blockevent.MIGRATION_DIRECOTRY,
+			params.MigrationRepoRef,
+		)
+		databaseURL := migrationhelper.GenerateDefaultDatabaseURL(name, connString)
+		migrationHelper := github_migrationhelper.NewGithubMigrationHelper(sourceURL, databaseURL)
+
+		return blockevent.NewBlockEvent(params.Logger, params.RdbConn, migrationHelper)
+	case "ChainStats":
+		sourceURL := github_migrationhelper.GenerateSourceURL(
+			github_migrationhelper.MIGRATION_GITHUB_URL_FORMAT,
+			params.GithubAPIUser,
+			params.GithubAPIToken,
+			chainstats.MIGRATION_DIRECOTRY,
+			params.MigrationRepoRef,
+		)
+		databaseURL := migrationhelper.GenerateDefaultDatabaseURL(name, connString)
+		migrationHelper := github_migrationhelper.NewGithubMigrationHelper(sourceURL, databaseURL)
+
+		return chainstats.NewChainStats(params.Logger, params.RdbConn, migrationHelper)
+	case "Proposal":
+		sourceURL := github_migrationhelper.GenerateDefaultSourceURL(name, githubMigrationHelperConfig)
+		databaseURL := migrationhelper.GenerateDefaultDatabaseURL(name, connString)
+		migrationHelper := github_migrationhelper.NewGithubMigrationHelper(sourceURL, databaseURL)
+
+		return proposal.NewProposal(params.Logger, params.RdbConn, params.ConsNodeAddressPrefix, migrationHelper)
+	case "Transaction":
+		sourceURL := github_migrationhelper.GenerateDefaultSourceURL(name, githubMigrationHelperConfig)
+		databaseURL := migrationhelper.GenerateDefaultDatabaseURL(name, connString)
+		migrationHelper := github_migrationhelper.NewGithubMigrationHelper(sourceURL, databaseURL)
+
+		return transaction.NewTransaction(params.Logger, params.RdbConn, migrationHelper)
+	case "Validator":
+		sourceURL := github_migrationhelper.GenerateDefaultSourceURL(name, githubMigrationHelperConfig)
+		databaseURL := migrationhelper.GenerateDefaultDatabaseURL(name, connString)
+		migrationHelper := github_migrationhelper.NewGithubMigrationHelper(sourceURL, databaseURL)
+
+		return validator.NewValidator(params.Logger, params.RdbConn, params.ConsNodeAddressPrefix, migrationHelper)
+	case "ValidatorStats":
+		sourceURL := github_migrationhelper.GenerateSourceURL(
+			github_migrationhelper.MIGRATION_GITHUB_URL_FORMAT,
+			params.GithubAPIUser,
+			params.GithubAPIToken,
+			validatorstats.MIGRATION_DIRECOTRY,
+			params.MigrationRepoRef,
+		)
+		databaseURL := migrationhelper.GenerateDatabaseURL(
+			connString,
+			validatorstats.MIGRATION_TABLE_NAME,
+		)
+		migrationHelper := github_migrationhelper.NewGithubMigrationHelper(sourceURL, databaseURL)
+
+		return validatorstats.NewValidatorStats(params.Logger, params.RdbConn, migrationHelper)
+	case "IBCChannel":
+		sourceURL := github_migrationhelper.GenerateDefaultSourceURL(name, githubMigrationHelperConfig)
+		databaseURL := migrationhelper.GenerateDefaultDatabaseURL(name, connString)
+		migrationHelper := github_migrationhelper.NewGithubMigrationHelper(sourceURL, databaseURL)
+
+		return ibc_channel.NewIBCChannel(
+			params.Logger,
+			params.RdbConn,
+			&ibc_channel.Config{
+				EnableTxMsgTrace: false,
+			},
+			migrationHelper,
+		)
+	case "IBCChannelTxMsgTrace":
+		sourceURL := github_migrationhelper.GenerateSourceURL(
+			github_migrationhelper.MIGRATION_GITHUB_URL_FORMAT,
+			params.GithubAPIUser,
+			params.GithubAPIToken,
+			ibc_channel.MIGRATION_DIRECOTRY,
+			params.MigrationRepoRef,
+		)
+		databaseURL := migrationhelper.GenerateDatabaseURL(
+			connString,
+			ibc_channel.MIGRATION_TABLE_NAME,
+		)
+		migrationHelper := github_migrationhelper.NewGithubMigrationHelper(sourceURL, databaseURL)
+
+		return ibc_channel.NewIBCChannel(
+			params.Logger,
+			params.RdbConn,
+			&ibc_channel.Config{
+				EnableTxMsgTrace: true,
+			},
+			migrationHelper,
+		)
+	case "IBCChannelMessage":
+		sourceURL := github_migrationhelper.GenerateDefaultSourceURL(name, githubMigrationHelperConfig)
+		databaseURL := migrationhelper.GenerateDefaultDatabaseURL(name, connString)
+		migrationHelper := github_migrationhelper.NewGithubMigrationHelper(sourceURL, databaseURL)
+
+		return ibc_channel_message.NewIBCChannelMessage(params.Logger, params.RdbConn, migrationHelper)
+	}
+
+	return nil
+}
+
+type InitProjectionParams struct {
+	Logger  applogger.Logger
+	RdbConn rdb.Conn
+
+	ExtraConfigs map[string]interface{}
+
+	CosmosAppClient       cosmosapp.Client
+	AccountAddressPrefix  string
+	ConsNodeAddressPrefix string
+
+	GithubAPIUser    string
+	GithubAPIToken   string
+	MigrationRepoRef string
+
+	ServerMigrationRepoRef string
+}
