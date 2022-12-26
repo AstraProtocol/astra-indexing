87d588ec5a2807f4866547c1c645af58fa5b8d96
trung <trung.nguyen25@tiki.vn>
2022-12-26 15:43:12 +0700

    migration: add total_gas_used + total_fee column

--- migrations/20221213161655_chain_stats.up.sql
+++ migrations/20221213161655_chain_stats.up.sql
@@ -1,4 +1,6 @@
 CREATE TABLE chain_stats (
     date_time BIGINT NOT NULL,
-    number_of_transactions INT NOT NULL
+    number_of_transactions INT DEFAULT 0 NOT NULL,
+    total_gas_used BIGINT DEFAULT 0 NOT NULL,
+    total_fee BIGINT DEFAULT 0 NOT NULL,
 );
