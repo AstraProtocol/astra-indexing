198eb8b62ab32217b2482fc64b1cc69743c1815e
trung <trung.nguyen25@tiki.vn>
2023-02-23 12:42:18 +0700

    fix: add ToAddress account to MsgCreateClawbackVestingAccount tx

--- projection/account_message/account_message.go
+++ projection/account_message/account_message.go
@@ -893,6 +893,7 @@ func (projection *AccountMessage) HandleEvents(height int64, events []event_enti
 				},
 				Accounts: []string{
 					typedEvent.Params.FromAddress,
+					typedEvent.Params.ToAddress,
 				},
 			})
 		}
