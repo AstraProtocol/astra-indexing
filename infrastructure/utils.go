f9bbebf896b9905ad9beff685e5ef3944d321f01
trung <trung.nguyen25@tiki.vn>
2023-05-08 23:43:25 +0700

    chore: using default kafka new data max wait

--- infrastructure/utils.go
+++ infrastructure/utils.go
@@ -6,6 +6,6 @@ const TIME_CACHE_FAST = 5 * time.Second
 const TIME_CACHE_MEDIUM = 30 * time.Second
 const TIME_CACHE_LONG = 10 * time.Minute
 
-const KAFKA_NEW_DATA_MAX_WAIT = time.Millisecond * 350
+const KAFKA_NEW_DATA_MAX_WAIT = time.Second * 10
 const KAFKA_TIME_OUT = time.Second * 3
 const KAFKA_READ_BATCH_TIME_OUT = time.Second * 60
