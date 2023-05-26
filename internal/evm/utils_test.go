8a15b0be0c6df7e67473a73d61c5c2397fa7cb51
trung <trung.nguyen25@tiki.vn>
2023-05-26 18:47:07 +0700

    refactor: update 4bytes.db

--- internal/evm/utils_test.go
+++ internal/evm/utils_test.go
@@ -21,35 +21,7 @@ func Test4Bytes(t *testing.T) {
 	fmt.Println("hoank", res)
 }
 
-func TestUpdate4Bytes(t *testing.T) {
-	assert := assert.New(t)
-
-	util, err := NewEvmUtils()
-	if err != nil {
-		panic(err)
-	}
-
-	util.UpdateSignature("0xd2aed2d6", "exchange(uint256[],uint256,bytes)")
-	util.UpdateSignature("0x1482dcda", "mintCoupons(address,uint256[])")
-	util.UpdateSignature("0xc4237db1", "createRewardProg(address,bytes)")
-	util.UpdateSignature("0x2b0e3b52", "createCouponProg(address,bytes)")
-	util.UpdateSignature("0x7a1b5ca3", "createRoleContract(bytes)")
-	util.UpdateSignature("0x3b0f6c11", "initQRDiscountProgTemplate(address,uint16,address)")
-	util.UpdateSignature("0xf2ac0d12", "initRoleTemplate(address,uint16)")
-	util.UpdateSignature("0xbfc05b8a", "initCouponProgTemplate(address,uint16,address)")
-	util.UpdateSignature("0xe9fdccd0", "initRewardProgTemplate(address,uint16,address)")
-	util.UpdateSignature("0xb134a366", "grantLockedReward(address[],uint256[],uint256[])")
-	util.UpdateSignature("0xf3fef3a3", "withdraw(address,uint256)")
-	util.UpdateSignature("0xa07aea1c", "addOperators(address[])")
-
-	res, err := util.GetSignature("0xbe739356")
-	if err != nil {
-		panic(err)
-	}
-	assert.Equal(res, "sendReward(address[],uint256[])")
-}
-
-func Test4GetSignatureFromData(t *testing.T) {
+func TestGetSignatureFromData(t *testing.T) {
 	assert := assert.New(t)
 
 	util, err := NewEvmUtils()
