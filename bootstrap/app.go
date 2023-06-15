3ded5eb57eb5edc25c6a9518ffb12eee78ced00f
trung <trung.nguyen25@tiki.vn>
2023-06-15 16:53:35 +0700

    refactor: add log

--- bootstrap/app.go
+++ bootstrap/app.go
@@ -120,13 +120,12 @@ func (a *app) Run() {
 				a.logger.Panicf("%v", runErr)
 			}
 		}()
-		/*
-			go func() {
-				if runErr := astra_consumer.RunConsumerInternalTxs(a.rdbConn.ToHandle(), a.config, a.logger); runErr != nil {
-					a.logger.Panicf("%v", runErr)
-				}
-			}()
-		*/
+		go func() {
+			if runErr := astra_consumer.RunConsumerInternalTxs(a.rdbConn.ToHandle(), a.config, a.logger); runErr != nil {
+				fmt.Println(runErr)
+				a.logger.Panicf("%v", runErr)
+			}
+		}()
 	}
 
 	select {}
