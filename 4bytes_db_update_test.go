d6641832dbc1b6859fd69afacb80b9a9cd5187ac
trung <trung.nguyen25@tiki.vn>
2023-06-26 14:00:59 +0700

    update 4bytes.db

--- 4bytes_db_update_test.go
+++ 4bytes_db_update_test.go
@@ -15,7 +15,7 @@ func TestUpdate4Bytes(t *testing.T) {
 		panic(err)
 	}
 
-	util.UpdateSignature("0x2494c055", "setCouponSupply(uint256)")
+	util.UpdateSignature("0x7ff36ab5", "swapExactETHForTokens(uint256, address[], address, uint256)")
 
 	res, err := util.GetSignature("0xa07aea1c")
 	if err != nil {
@@ -23,8 +23,8 @@ func TestUpdate4Bytes(t *testing.T) {
 	}
 	assert.Equal(res, "addOperators(address[])")
 
-	input := "0x2494c055"
-	assert.Equal(util.GetMethodNameFromMethodId(input[2:10]), "setCouponSupply")
+	input := "0x7ff36ab5"
+	assert.Equal(util.GetMethodNameFromMethodId(input[2:10]), "swapExactETHForTokens")
 
 	data := "0q7S1gAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAGSWzhIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACcAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQVbZvQpCuU8VUVDSeyAd243bdr5ljiPA3bK7aUm/VuQVdhajtudaHAI7ltLe1GHQeZchiXxaXAq2Ez+6ErCpJRscAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=="
 	signature := util.GetMethodNameFromData(data)
