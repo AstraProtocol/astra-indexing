c620fc5a13385fcabe8b2c59de777beabde8fffb
trung <trung.nguyen25@tiki.vn>
2023-08-04 16:42:13 +0700

    fix: token price add error handler

--- infrastructure/jsonrpc/common_response.go
+++ infrastructure/jsonrpc/common_response.go
@@ -3,5 +3,6 @@ package jsonrpc
 type CommonResp struct {
 	Jsonrpc string      `json:"jsonrpc"`
 	Id      uint        `json:"id"`
-	Result  interface{} `json:"result"`
+	Result  interface{} `json:"result,omitempty"`
+	Error   interface{} `json:"error,omitempty"`
 }
